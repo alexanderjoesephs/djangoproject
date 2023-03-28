50328fbd42dc8f92450e135bb734accbdd1fc677
unknown <alexandersonkin@gmail.com>
2023-03-28 22:20:18 +0100

    fds

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS=["https://web-production-30c1.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS=["https://web-production-30c1.up.railway.app"]
